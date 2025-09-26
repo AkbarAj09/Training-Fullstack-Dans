7ff94d2c7e00f6c64b74776e27b0005f2b71a76c
AkbarAj09 <210210502010@student.unm.ac.id>
2025-09-26 15:32:11 +0700

    update project express js

--- generateName.js
+++
@@ -1,9 +0,0 @@
-// import generateName from 'sillyname';
-import {randomSuperhero} from 'superheroes';
-
-// const sillyname = generateName();
-// console.log(sillyname);
-
-const heroName = randomSuperhero();
-console.log(heroName);
-
