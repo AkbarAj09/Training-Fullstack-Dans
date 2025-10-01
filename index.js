e2c4d432fb2a7a16605e140e808115910cefbb4b
AkbarAj09 <210210502010@student.unm.ac.id>
2025-10-01 11:20:34 +0700

    deploy ke production vercel

--- index.js
+++ index.js
@@ -1,23 +1,69 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import express from "express";
+import { dirname } from "path";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+import morgan from "morgan";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// Middleware untuk melayani file statis dari folder 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  res.send(`Login attempted! Username: ${username}, Password: ${password}`);
+app.post('/home', (req, res) => {
+    const { username, password } = req.body;
+    if (password === "123") {
+        const now = new Date();
+        const hour = now.getHours();
+        let greeting;
+
+        if (hour >= 5 && hour < 11) {
+            greeting = "selamat pagi";
+        } else if (hour >= 11 && hour < 15) {
+            greeting = "selamat siang";
+        } else if (hour >= 15 && hour < 18) {
+            greeting = "selamat sore";
+        } else {
+            greeting = "selamat malam";
+        }
+
+        // Mengirim HTML dinamis sebagai respons
+        res.status(200).send(`
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>HomePage Admin</title>
+            </head>
+            <body>
+                <h1>Welcome to the Admin HomePage</h1>
+                <h2>Halo ${username ? `${username}` : 'Admin'}, ${greeting}</h2>
+            </body>
+            </html>
+        `);
+    } else {
+        res.status(401).send("Invalid password!");
+        res.redirect('/');
+    }
 });
 
-app.get('/', (req, res) => {
-    console.log(__dirname);
-  res.sendFile(__dirname + "/public/index.html");
+app.get("/", (_req, res) => {
+    // Arahkan ke file index.html di dalam folder public
+    res.status(200);
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Jalankan server HANYA untuk development lokal
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}
+
+
+export default app;
